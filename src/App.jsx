79293557813e44e2de098d57d886e69aef5ff00a
Simon Moisselin (mini) <simon.moisselin@gmail.com>
2023-11-10 08:49:39 -1000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import ReactMarkdown from 'react-markdown';
+
+
+
+const text = `This is a great start to your resume, Simon! Your experience in the field of machine learning and data analysis is well-detailed and shows a solid trajectory in your career. However, there are a few minor adjustments I'd like to suggest for clarity and accuracy:\n\n- Django React JS, Celery Docker => Django, ReactJS, Celery, Docker\n- Informatics Modules: Python, Matlab, Web => Informatics Modules: Python, MATLAB, Web Development\n- Bachelor’s degree Economics and Mathematics => Bachelor's Degree in Economics and Mathematics\n- Paper Publication - Diagnosis of giant cell arteritis with Deep Learning => Research Publication: Diagnosis of Giant Cell Arteritis Using Deep Learning\n\nNow, let's grade each part of the resume:\n\n1. Contact Information: A\nEverything is clearly listed and easily accessible. Great job here!\n\n2. Experience: A-\nVery impressive work experience! Consider using bullet points consistently for all job descriptions to improve readability.\n\n3. Education: A\nYour educational background is relevant and well-presented. Just make sure to maintain consistency in the formatting (e.g., capitalization and degree titles).\n\n4. Skills: B+\nYour skills are relevant and well-suited for the positions you're interested in. Consider separating 'Software' and 'Machine Learning' into different categories for clearer distinction.\n\n5. Side Projects: B\nFor 'Side Projects,' provide a bit more detail or context. For instance:\n- Summarize podcasts and books with GPT-3 + Whisper => Developed a summarization tool using GPT-3 and Whisper for podcasts and books\n- Pynanote => (Provide a brief description if Pynanote is a project or tool you created or contributed to)\n\nLastly, while not listed as a category, the overall formatting could use a slight polish. Align dates properly and ensure that the spacing between sections is consistent.\n\nRemember to tailor your resume for each job application to highlight the most relevant experience and skills for the position. You're on a strong path, Simon, and with these minor tweaks, your resume will be even more effective at showcasing your professional strengths. Keep up the good work!`
 
 const ResumeUploader = () => {
   const [resume, setResume] = useState(null);
@@ -62,12 +66,8 @@ const ResumeUploader = () => {
       </div>
 
       <div className="w-full max-w-xl">
-        <div className="markdown bg-white p-4 rounded shadow h-64">
-          {reviewMarkdown ? (
-            <div dangerouslySetInnerHTML={{ __html: reviewMarkdown }} />
-          ) : (
-            <p className="text-gray-400">Your resume review will appear here...</p>
-          )}
+        <div className="markdown bg-white p-4 rounded shadow h-64 overflow-auto">
+          <ReactMarkdown>{text || "Your resume review will appear here..."}</ReactMarkdown>
         </div>
       </div>
     </div>
